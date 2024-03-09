235b633568ed5b562ebfd3a4c7b72d86f79ae645
nphucgiabao <nphucgiabao@gmail.com>
2024-03-10 00:31:04 +0700

    edit

---
+++ server/api/api/Identity/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Identity
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
--- server/api/api/Startup.cs
+++ server/api/api/Startup.cs
@@ -1,7 +1,10 @@
+using api.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +28,15 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddControllers();
 
             services.AddSwaggerGen(c =>
