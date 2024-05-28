8193eb311fa47c7087c1dfbd3e618ff92cb23b74
GiaBao1999@bitbucket.org <nphucgiabao@gmail.com>
2024-05-28 17:04:16 +0700

    edit

---
+++ server/api/user_api/Migrations/20240528061046_UpdateTableAspNetUsers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace user_api.Migrations
+{
+    public partial class UpdateTableAspNetUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+               name: "OId",
+               table: "AspNetUsers",
+               nullable: true,
+               type: "varchar",
+               maxLength: 100);
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OId",
+                table: "AspNetUsers");
+        }
+    }
+}
